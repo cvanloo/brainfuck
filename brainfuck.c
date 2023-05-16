e3410f37450899c2cc33320f94a0c2b30f5a5ef4
Colin van Loo <colin.vanloo@edu.tbz.ch>
2023-05-16 15:33:01 +0200

    ref: check that we don't overflow loop

--- brainfuck.c
+++ brainfuck.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 
 #define CHUNK_SIZE 512
+#define MAX_LOOPS 10
 
 int main(int argc, char *argv[]) {
     char *program, *input;
@@ -13,30 +14,24 @@ int main(int argc, char *argv[]) {
     if (isatty(fileno(stdin))) {
         assert(argc > 1 && "Invalid number of arguments.");
         program = argv[1];
-        if (argc > 2) {
-            input = argv[2];
-        }
+        if (argc > 2) input = argv[2];
     } else {
         char *tmp_program = malloc(CHUNK_SIZE);
         size_t total_read = 0;
         while (1) {
             ssize_t nb = read(fileno(stdin), tmp_program + total_read, CHUNK_SIZE);
-            if (nb == 0)
-                break;
+            if (nb == 0) break;
             total_read += nb;
             tmp_program = realloc(tmp_program, total_read + CHUNK_SIZE);
         }
         program = tmp_program;
-
-        if (argc > 1) {
-            input = argv[1];
-        }
+        if (argc > 1) input = argv[1];
     }
 
     char stack[100] = {0};
     size_t ptr = 0;
 
-    char *loops[10];
+    char *loops[MAX_LOOPS];
     int8_t current_loop = -1;
     char *instruction = program;
     while (*instruction != 0) {
@@ -57,20 +52,20 @@ int main(int argc, char *argv[]) {
             if (stack[ptr] == 0) {
                 size_t top = 1;
                 for (size_t i = 0; i < top; ++i) {
-                    while (*(++instruction) != ']') {
-                        if (*instruction == '[') {
-                            ++top;
-                        }
-                    }
+                    while (*(++instruction) != ']')
+                        if (*instruction == '[') ++top;
                 }
+                // Continue executing after loop end (++instruction after switch end).
             } else {
                 ++current_loop;
+                assert(current_loop < MAX_LOOPS && "Exhausted loop stack.");
                 loops[current_loop] = instruction;
             }
             break;
         case ']':
             if (stack[ptr] != 0) {
                 instruction = loops[current_loop];
+                // Continue executing after loop start (++instruction after switch end).
             } else {
                 --current_loop;
             }
