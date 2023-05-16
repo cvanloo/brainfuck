46bbe3d47acb4c798c2808d3ee668239b836f1f3
Colin van Loo <colin.vanloo@edu.tbz.ch>
2023-05-16 16:32:49 +0200

    ref: preprocess loops

--- brainfuck.c
+++ brainfuck.c
@@ -2,28 +2,54 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 #define CHUNK_SIZE 512
 #define STACK_SIZE 100
-#define MAX_LOOPS 10
+
+enum Direction { Forward, Backward };
+
+char *find_match(char *start, char match, enum Direction dir) {
+    char *ip = start;
+    size_t count = 1;
+    while (count) {
+        dir == Forward ? ++ip : --ip;
+        if (*ip == *start) ++count;
+        if (*ip == match) --count;
+    }
+    return ip;
+}
+
+void preprocess_loops(char *loops[], char *program) {
+    char *ip = program;
+    while (*ip != 0) {
+        if (*ip == '[') {
+            loops[ip - program] = find_match(ip, ']', Forward);
+        } else if (*ip == ']') {
+            loops[ip - program] = find_match(ip, '[', Backward);
+        }
+        ++ip;
+    }
+}
 
 int main(int argc, char *argv[]) {
     char *program, *input;
+    size_t program_length = 0;
 
     if (isatty(fileno(stdin))) {
         assert(argc > 1 && "Invalid number of arguments.");
         program = argv[1];
+        program_length = strlen(program);
         if (argc > 2) input = argv[2];
     } else {
         char *tmp_program = malloc(CHUNK_SIZE);
-        size_t total_read = 0;
         while (1) {
-            ssize_t nb = read(fileno(stdin), tmp_program + total_read, CHUNK_SIZE);
+            ssize_t nb = read(fileno(stdin), tmp_program + program_length, CHUNK_SIZE);
             if (nb == 0) break;
-            total_read += nb;
-            tmp_program = realloc(tmp_program, total_read + CHUNK_SIZE);
+            program_length += nb;
+            tmp_program = realloc(tmp_program, program_length + CHUNK_SIZE);
         }
         program = tmp_program;
         if (argc > 1) input = argv[1];
@@ -31,12 +57,12 @@ int main(int argc, char *argv[]) {
 
     char stack[STACK_SIZE] = {0};
     char *ptr = stack;
+    char *loops[program_length];
+    preprocess_loops(loops, program);
 
-    char *loops[MAX_LOOPS];
-    int8_t current_loop = -1;
-    char *instruction = program;
-    while (*instruction != 0) {
-        switch (*instruction) {
+    char *ip = program;
+    while (*ip != 0) {
+        switch (*ip) {
         case '+':
             ++(*ptr);
             break;
@@ -50,26 +76,10 @@ int main(int argc, char *argv[]) {
             --ptr;
             break;
         case '[':
-            if (*ptr == 0) {
-                size_t top = 1;
-                for (size_t i = 0; i < top; ++i) {
-                    while (*(++instruction) != ']')
-                        if (*instruction == '[') ++top;
-                }
-                // Continue executing after loop end (++instruction after switch end).
-            } else {
-                ++current_loop;
-                assert(current_loop < MAX_LOOPS && "Exhausted loop stack.");
-                loops[current_loop] = instruction;
-            }
+            if (!*ptr) ip = loops[ip - program];
             break;
         case ']':
-            if (*ptr != 0) {
-                instruction = loops[current_loop];
-                // Continue executing after loop start (++instruction after switch end).
-            } else {
-                --current_loop;
-            }
+            if (*ptr) ip = loops[ip - program];
             break;
         case '.':
             printf("%c", *ptr);
@@ -83,7 +93,7 @@ int main(int argc, char *argv[]) {
             }
             break;
         }
-        ++instruction;
+        ++ip;
     }
 
     return EXIT_SUCCESS;
